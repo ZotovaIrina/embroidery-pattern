8dbf6e76a6d937278bf68c5b3f4d8f2a63b97bf9
ZotovaIrina <ZotovaIrina@users.noreply.github.com>
2016-04-30 15:01:27 +0300

    static server

---
+++ server/path.js
@@ -0,0 +1,6 @@
+var path = require('path');
+module.exports = {
+    serverDir: path.join(__dirname),
+    clientDir: path.join(__dirname, './../client'),
+    publicDir: path.join(__dirname, './public')
+};
