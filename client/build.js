cc116b80549f5cb929d1b82b469082c74fc6956a
ZotovaIrina <ZotovaIrina@users.noreply.github.com>
2016-06-13 09:13:24 +0300

    Limit maxWidth

--- client/build.js
+++ client/build.js
@@ -403,12 +403,18 @@ angular.module('embroidery-pattern', ['ui.router', 'ngResource', 'ngAnimate', 'n
                     // success, "onload" catched
                     // now we can do specific stuff:
                     imageParams.heightImage = parseInt(this.naturalHeight*0.9);
-
                     imageParams.widthImage = parseInt(this.naturalWidth*0.9);
                     imageParams.maxWidth = this.naturalWidth;
                     imageParams.maxHeigth = this.naturalHeight;
                     imageParams.proportion = this.naturalHeight / this.naturalWidth;
                     imageParams.numberOfColor = 20;
+                     if(imageParams.maxWidth > 600) {
+                         imageParams.maxWidth = 600;
+                         imageParams.maxHeigth = imageParams.proportion * imageParams.maxWidt;
+                         imageParams.widthImage = imageParams.maxWidth * 0.9;
+                         imageParams.heightImage = imageParams.maxHeigth * 0.9;
+
+                     }
                     scope.imageOnLoad({result: imageParams});
                 });
 
